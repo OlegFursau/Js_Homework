69b258bf5f403e8666b6f5a1c610790bbe6e302d
Oleg <93030888+multik8711@users.noreply.github.com>
2022-01-20 19:00:41 +0300

    1011 commit

--- homework_10/app2.js
+++ homework_10/app2.js
@@ -1,48 +1,51 @@
-// function Animal(name) {
-//     this._name = name;
+function Animal(name) {
+    this._name = name;
+    var self = this;
 
 
-// }
-// Animal.prototype.dailyNorm = function(amount) {
-//     if (!arguments.length) return foodAmount;
-//     if (amount == 0 || amount < 50) throw new Error(this._name + ' будет очень голодный');
-//     if (amount > 100) throw new Error(this._name + ' объестся и лопнет! нельзя ему такое количество корма');
-//     foodAmount = amount;
-// }
-// Animal.prototype.formatFoodAmount = function(foodAmount) {
-//     return foodAmount + ' гр';
-// }
-// Animal.prototype.feed = function() {
-//     console.log(`Насыпаем в миску ${this._name}a` + ' ' + this.dailyNorm() + ' корма.');
-// }
+}
+Animal.prototype.dailyNorm = function(amount) {
+    if (!arguments.length) return foodAmount;
+    if (amount == 0 || amount < 50) throw new Error(this._name + ' будет очень голодный');
+    if (amount > 100) throw new Error(this._name + ' объестся и лопнет! нельзя ему такое количество корма');
+    self.foodAmount = amount;
+    return this._formatFoodAmount(foodAmount);
+}
 
-// function Cat() {
-//     Animal.apply(this, arguments);
-// }
-// Cat.prototype = Object.create(Animal.prototype);
-// Cat.prototype.constructor = Cat;
-// Cat.prototype.feed = function() {
-//     Animal.prototype.feed.apply(this, arguments);
-//     console.log('Кот доволен ^_^');
-//     return this;
-// }
-// Cat.prototype.stoke = function() {
-//     console.log('Гладим кота.');
-//     return this;
-// }
-// var tigr = new Cat('Тигр')
+Animal.prototype._formatFoodAmount = function() {
+    return self.foodAmount + ' гр';
+}
+Animal.prototype.feed = function() {
+    console.log(`Насыпаем в миску ${this._name}a` + ' ' + this.dailyNorm() + ' корма.');
+}
 
-
-// tigr.dailyNorm(30);
-// tigr.feed().stoke()
+function Cat() {
+    Animal.apply(this, arguments);
+}
+Cat.prototype = Object.create(Animal.prototype);
+Cat.prototype.constructor = Cat;
+Cat.prototype.feed = function() {
+    Animal.prototype.feed.apply(this, arguments);
+    console.log('Кот доволен ^_^');
+    return this;
+}
+Cat.prototype.stoke = function() {
+    console.log('Гладим кота.');
+    return this;
+}
+var tigr = new Cat('Тигр')
 
 
+tigr.dailyNorm(50);
+tigr.feed().stoke()
 
-// Задание 2:
-//     Написать функцию, возвращающую глубокую копию объекта - его клон. Клонироваться должны значения всех типов данных
-//     (+ массивы и функции; NaN не учитывать), а также любого уровня вложенности. Метод isArray использовать можно.
 
-//     Протестировать работу функции можно на таком примере:
+
+// // Задание 2:
+// //     Написать функцию, возвращающую глубокую копию объекта - его клон. Клонироваться должны значения всех типов данных
+// //     (+ массивы и функции; NaN не учитывать), а также любого уровня вложенности. Метод isArray использовать можно.
+
+// //     Протестировать работу функции можно на таком примере:
 
 // var initialObj = {
 //     string: 'Vasya',
@@ -92,78 +95,78 @@
 
 
 
-// задание 3 Написать функцию глубокого сравнения объектов, возвращающую boolean. Сравниваться должны значения всех типов данных
-// (+ массивы и функции; NaN не учитывать), а также любого уровня вложенности. Для определения длины объектов
-// разрешается использовать метод Object.keys(). Хорошо протестировать работу функции (можно на объекте из пред. задания).
+// // // задание 3 Написать функцию глубокого сравнения объектов, возвращающую boolean. Сравниваться должны значения всех типов данных
+// // // (+ массивы и функции; NaN не учитывать), а также любого уровня вложенности. Для определения длины объектов
+// // // разрешается использовать метод Object.keys(). Хорошо протестировать работу функции (можно на объекте из пред. задания).
 
 
 
-function getDeepEqual(obj) {
-    return Object.prototype.toString.call(obj);
+// function getDeepEqual(obj) {
+//     return Object.prototype.toString.call(obj);
 
-}
+// }
 
-function deepEqual(a, b) {
-    if (a === b) return true;
-    if (typeof(a) != typeof(b)) return false;
-    var aDeep = getDeepEqual(a);
-    var bDeep = getDeepEqual(b);
-    if (aDeep != bDeep) return false;
-    if (aDeep == '[object Boolean]' || aDeep == '[object String]' || aDeep == '[object Number]') {
-        if (a.valueOf() != b.valueOf()) return false
-    }
-    if (aDeep == '[object RegExp]' || aDeep == '[object Date]' || aDeep == '[object Error]') {
-        if (a.toString() != b.toString()) return false;
-    }
-    if (aDeep == '[object Function]' && a.toString() != b.toString()) return false;
-    var aKeys = Object.keys(a);
-    var bKeys = Object.keys(b);
-    if (aKeys.length != bKeys.length) return false;
-    if (!aKeys.every(function(key) { return b.hasOwnProperty(key) })) return false;
-    return aKeys.every(function(key) {
-        return deepEqual(a[key], b[key])
-    });
-
-
-
-}
+// function deepEqual(a, b) {
+//     if (a === b) return true;
+//     if (typeof(a) != typeof(b)) return false;
+//     var aDeep = getDeepEqual(a);
+//     var bDeep = getDeepEqual(b);
+//     if (aDeep != bDeep) return false;
+//     if (aDeep == '[object Boolean]' || aDeep == '[object String]' || aDeep == '[object Number]') {
+//         if (a.valueOf() != b.valueOf()) return false
+//     }
+//     if (aDeep == '[object RegExp]' || aDeep == '[object Date]' || aDeep == '[object Error]') {
+//         if (a.toString() != b.toString()) return false;
+//     }
+//     if (aDeep == '[object Function]' && a.toString() != b.toString()) return false;
+//     var aKeys = Object.keys(a);
+//     var bKeys = Object.keys(b);
+//     if (aKeys.length != bKeys.length) return false;
+//     if (!aKeys.every(function(key) { return b.hasOwnProperty(key) })) return false;
+//     return aKeys.every(function(key) {
+//         return deepEqual(a[key], b[key])
+//     });
 
 
-var a = {
-    string: 'Vasya',
-    number: 30,
-    boolean: true,
-    undefined: undefined,
-    null: null,
-    array: [1, 2, 3],
-    object: {
-        string2: 'Petrov',
-        object2: {
-            array2: [{}, {}]
-        },
-        object3: {}
-    },
-    method: function() {
-        alert('Hello');
-    }
-};
-var b = {
-    string: 'Vasya',
-    number: 30,
-    boolean: true,
-    undefined: undefined,
-    null: null,
-    array: [1, 2, 3, ],
-    object: {
-        string2: 'Petrov',
-        object2: {
-            array2: [{}, {}]
-        },
-        object3: {}
-    },
-    method: function() {
-        alert('Hello');
-    }
-};
 
-console.log(deepEqual(a, b));
+// }
+
+
+// var a = {
+//     string: 'Vasya',
+//     number: 30,
+//     boolean: true,
+//     undefined: undefined,
+//     null: null,
+//     array: [1, 2, 3],
+//     object: {
+//         string2: 'Petrov',
+//         object2: {
+//             array2: [{}, {}]
+//         },
+//         object3: {}
+//     },
+//     method: function() {
+//         alert('Hello');
+//     }
+// };
+// var b = {
+//     string: 'Vasya',
+//     number: 30,
+//     boolean: true,
+//     undefined: undefined,
+//     null: null,
+//     array: [1, 2, 3, ],
+//     object: {
+//         string2: 'Petrov',
+//         object2: {
+//             array2: [{}, {}]
+//         },
+//         object3: {}
+//     },
+//     method: function() {
+//         alert('Hello');
+//     }
+// };
+
+// console.log(deepEqual(a, b));
