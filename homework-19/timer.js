167b4dbdbaccbdc22faf9ec018be815da89bbe40
Oleg <afursau@gmail.com>
2022-02-20 16:45:48 +0300

    homework_19fix*

--- homework-19/timer.js
+++ homework-19/timer.js
@@ -1,7 +1,6 @@
 // !ДОМАШНЕЕ ЗАДАНИЕ
 // *Задание 1:
 //     ?Скинуть идею итогового проекта и краткое описание его функционала.
-
 // *Задание 2:
 //     ///?Написать свой секундомер в формате mm: ss: msms(по 2 цифры в каждом параметре).
 // ///? Изначально на странице должна быть кнопка "Start".При запуске секундомера текст кнопки меняется на "Stop".
@@ -19,15 +18,12 @@
 //* на каждой итерации и считать их до 100.
 // !При реализации класс Date использовать запрещено.
 
-
-
 document.addEventListener('DOMContentLoaded', () => {
-
-
     //* varible and Additional
-    const body = document.body;
     const btn = document.getElementsByClassName('btn')[0];
     const div = document.getElementsByClassName('timer_start')[0];
+    const span = document.getElementsByTagName('span');
+
     let stopWatch = document.createElement('div');
     stopWatch.className = 'timer';
     stopWatch.innerHTML = `   <div class="min">
@@ -43,62 +39,65 @@ document.addEventListener('DOMContentLoaded', () => {
     const minutes = document.getElementById('min');
     const seconds = document.getElementById('sec');
     const msms = document.getElementById('ms');
-    let button
+    let button;
     let timer;
     let zero = [0, 0, 0];
     let [min, sec, ms] = zero;
     let reset;
     let save;
     let i = 1;
-
-    //*additional
-
-    // localStorage.getItem('min') == null && localStorage.getItem('sec') == null && localStorage.getItem('ms') == null ? [min, sec, ms] = zero :
-    //     min = localStorage.getItem('min'),
-    //     sec = localStorage.getItem('sec'),
-    //     ms = localStorage.getItem('ms');
-
     //*addEventList
     btn.addEventListener('click', checkButton);
 
     //* Funtions
-
     function checkButton() {
         event.stopPropagation();
         if (btn.dataset.state === 'Start') {
             btn.innerHTML = 'Stop'
             btn.dataset.state = 'Stop'
+            btn.classList.add('active')
             startStopWatch()
         } else if (btn.dataset.state == 'Stop') {
             btn.innerHTML = 'Run';
             btn.dataset.state = 'Run';
             clearInterval(timer);
+            btn.classList.remove('active');
+            btn.classList.add('run')
         } else if (btn.dataset.state === 'Run') {
             btn.innerHTML = 'Stop'
             btn.dataset.state = 'Stop'
             startStopWatch()
             button.remove()
+            btn.classList.remove('run')
+            btn.classList.add('active');
+
         }
     }
 
     function startStopWatch() {
-
         timer = setInterval(() => {
-            localStorage.setItem('ms', ms++);
-            ms < 9 ? msms.innerText = `0${localStorage.getItem('ms')}` : msms.innerText = `${localStorage.getItem('ms')}`;
+            ms++
+            ms < 9 ? msms.innerText = `0${ms}` : msms.innerText = `${ms}`;
             if (ms === 99) {
-                ms = 0
-                localStorage.setItem('sec', sec++);
-                sec <= 9 ? seconds.innerText = `0${localStorage.getItem('sec')}` : seconds.innerText = `${localStorage.getItem('sec')}`
+                ms = 0;
+                sec++
+                sec <= 9 ? seconds.innerText = `0${sec}` : seconds.innerText = `${sec}`
             }
-            if (sec == 59) {
-                sec = 0;
-                localStorage.setItem('min', min++);
-                min <= 9 ? minutes.innerText = `0${localStorage.getItem('min')}` : minutes.innerText = `${localStorage.getItem('min')}`
-            } else if (localStorage.getItem('min') == 60 && localStorage.getItem('sec') == 0 && localStorage.getItem('ms') == 0) {
+            if (sec === 60) {
+                seconds.innerText = '00';
+                sec = 0
+                min++
+                min <= 9 ? minutes.innerText = `0${min}` : minutes.innerText = `${min}`
+            } else if (min === 60) {
+                min < 9 ? minutes.innerText = `0${min}` : `${min}`;
+                seconds.innerText = '00';
+                msms.innerText = '00';
                 clearInterval(timer);
                 btn.remove()
                 save.remove();
+                for (let i in span) {
+                    span[i].className = 'red'
+                }
             }
         }, 10);
         addButton()
@@ -111,28 +110,17 @@ document.addEventListener('DOMContentLoaded', () => {
                             <div class="timer_stop">
                                 <button id ="save" class="btnStop" data-state="Save">Save</button>
                             </div>
-                            <div class="result">
-                                    
+                            <div class="result">     
                             </div>`;
         stopWatch.insertAdjacentElement('afterend', button);
         reset = document.getElementById('reset');
         save = document.getElementById('save');
-        reset.onclick = resetStopWatch
+        reset.onclick = resetStopWatch;
         save.onclick = saveStopWatch;
     }
 
     function resetStopWatch() {
         location.reload(); //думаю что так будет надежнее 
-        localStorage.clear();
-        // clearInterval(timer);
-        // [min, sec, ms] = zero
-        // minutes.innerText = `0${min}`;
-        // seconds.innerText = `0${sec}`;
-        // msms.innerText = `0${ms}`;
-        // btn.innerHTML = 'Start';
-        // btn.dataset.state = 'Start';
-        // if (btn.dataset.state == 'Start' || btn.dataset.state == 'Run')
-        //     button.remove();
     }
 
     function saveStopWatch() {
@@ -142,8 +130,15 @@ document.addEventListener('DOMContentLoaded', () => {
         if (localStorage.getItem('min') == null) {
             localStorage.setItem('min', 0)
         }
-        localStorage.getItem('min') < 9 && localStorage.getItem('sec') < 9 ? p.innerText = `${i}) 0${localStorage.getItem('min')} : 0${localStorage.getItem('sec')} : 0${localStorage.getItem('ms')}` :
-            p.innerText = `${i}) ${localStorage.getItem('min')} : ${localStorage.getItem('sec')} : ${localStorage.getItem('ms')}`;
+        if (sec <= 9) {
+            p.innerText = `${i}) 0${min} : 0${sec} : ${ms}`;
+        } else if (sec > 9 && min == 0) {
+            p.innerText = `${i}) 0${min} : ${sec} : ${ms}`
+        } else if (sec > 9 && min < 9) {
+            p.innerText = `${i}) 0${min} : ${sec} : ${ms}`;
+        } else {
+            p.innerText = `${i}) ${min} : ${sec} : ${ms}`;
+        }
         result.appendChild(p)
         return i++
     }
